ae6f63db4541f189c5a1d23644903de023907485
Torstein Bratberg <tdbratberg@gmail.com>
2020-05-17 13:51:06 +0200

    Add project files.

---
+++ CsFromScratch2/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace CsFromScratch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohoyhoy world");
+            Console.ReadKey();
+            //CreateWebHostBuilder(args).Build().Run();
+            //Debug.WriteLine("Ohoyhoy world");
+            //Console.WriteLine(args[0]);
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+        }
+    }
+}
---
+++ CsFromScratch2/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.HttpsPolicy;
+//using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace CsFromScratch
+{
+    class Startup
+    {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Use(async (context, next) =>
+                {
+                    await context.Response.WriteAsync("Ohoyhoy");
+                    await next();
+            });
+        }
+    }
+}
